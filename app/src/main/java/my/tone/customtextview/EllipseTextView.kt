72d018e165a51ed059b430d7d2e53580a0b5c6b4
Charko <eeodyd@momocon.co.kr>
2021-02-16 14:23:26 +0900

    더보기 기능

--- app/src/main/java/my/tone/customtextview/EllipseTextView.kt
+++ app/src/main/java/my/tone/customtextview/EllipseTextView.kt
@@ -1,12 +1,16 @@
 package my.tone.customtextview
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Build
+import android.text.Spannable
+import android.text.style.ForegroundColorSpan
 import android.util.AttributeSet
 import android.util.Log
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.toSpannable
 import java.lang.Integer.min
 
 class EllipseTextView @JvmOverloads constructor(
@@ -16,7 +20,7 @@ class EllipseTextView @JvmOverloads constructor(
 ) :
     AppCompatTextView(context, attr, defStyleAttr) {
 
-    var moreText = "(10)"
+    var moreText: String? = null
     var originText = ""
 
     init {
@@ -25,7 +29,9 @@ class EllipseTextView @JvmOverloads constructor(
             override fun onGlobalLayout() {
                 originText = text.toString()
 
-                text = "${text} ${moreText}"
+                text = moreText?.let {
+                    "$text $moreText"
+                } ?: text
 
                 if (maxLines >= lineCount)
                     addCustomMore()
@@ -42,11 +48,6 @@ class EllipseTextView @JvmOverloads constructor(
     // 연속된 글자 작성시
 
     private fun addCustomMore() {
-        Log.e(">>>>>>>>", "settext22 : ${text}")
-        Log.e(">>>>>>>>", "settext length : ${text.length}")
-        Log.e(">>>>>>>>", "lineCount22 : ${lineCount}")
-        Log.e(">>>>>>>>", "viewLines : ${maxLines}")
-
         var start = 0
         var end = 0
         var showText = ""
@@ -62,24 +63,38 @@ class EllipseTextView @JvmOverloads constructor(
                 start = end
             } else {
                 var isEllipsis = layout.getEllipsisCount(i)
-                end += if(isEllipsis > 0) lineLength ?: 0 else text.length
+                end += if (isEllipsis > 0) lineLength ?: 0 else {
+                    showText = text.toString()
+                    break
+                }
 
                 if (start < end && end <= text.length)
-                    showText += originText.substring(start, Math.min(end, originText.length))
+                    showText += text.substring(start, Math.min(end, originText.length))
             }
         }
 
-        if (originText.length > showText.length) {
-            // ...
-            showText = showText.substring(0, showText.length - (moreText.length + 4))
+        if (originText.length >= showText.length) {
+            showText = showText.substring(0, showText.length - (moreText?.length ?: -1 + 4))
 
-            text = "${showText}... ${moreText}"
+            text = "$showText...${moreText?.let { " $moreText" } ?: ""}"
         }
 
-        Log.e(">>>>>>>>", "show Text : ${showText}")
-        Log.e(">>>>>>>>", "originText : ${originText}")
-        Log.e(">>>>>>>>", "originText.length : ${originText.length}")
-        Log.e(">>>>>>>>", "showText.length : ${showText.length}")
 
+        moreText?.let {
+            text = moreSpan(it)
+        }
+    }
+
+    private fun moreSpan(moreText: String): Spannable {
+        var span = text.toSpannable()
+
+        span.setSpan(
+            ForegroundColorSpan(Color.parseColor("#FF8983")),
+            text.length - moreText.length,
+            text.length,
+            Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+
+        return span
     }
 }
--- app/src/main/java/my/tone/customtextview/MainActivity.kt
+++ app/src/main/java/my/tone/customtextview/MainActivity.kt
@@ -2,6 +2,7 @@ package my.tone.customtextview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         text_tv.text =
-            "123456789!123456789@123456789#123456789\$123456789%12345678"
+//            "123456789!123456789@123456789#123456789$123456789%12341341" //어중간한 숫자 값일 때
+//            "가나다라마바사아자차카타하일이삼사오육칠팔구십하나두울세엣네엣다섯여섯일곱여덟아홉열열하나열둘열셋" //어중간한 한글 값일 때
+//        "가나다라마바사아자차카타하일이삼사오육칠팔구십하나두울세엣네엣다섯여섯일곱여덟아홉여얼기억니은디긋리을미음비읍시옷이응지읏치읏키억티긋히읏" // 한글 길때
+//        "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz" // 영어 길때
+//        "abcdefghijklmnopqrstuvwxy가나다라마바사아자차카타하일이삼사오육칠팔구십하나두울세엣네엣다섯여섯일" // 영어 + 한글
+            "///////////////////////////////////abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy" // 한글 + 영어
+        text_tv.moreText = "(10)"
+
+//        Pattern.matches - "^[0-9a-zA-Z가-힣]*$"
+//
+
+        Log.e(">>>>>>", ">>>>>>>>> 가 ${'가'.toInt()}")
+        Log.e(">>>>>>", ">>>>>>>>> ㄱ ${'ㄱ'.toInt()}")
+        Log.e(">>>>>>", ">>>>>>>>> ㄴ ${'ㄴ'.toInt()}")
+
+        // 해당 한글을 찾아 pure length를 찾는다.
+        // pure length 는(영어 or 숫자, asc 값 범위)기준 값을 말한다.
+        // 영어 숫자 대비 한글의 경우 1.5 수준을 가진다.
 
     }
 }
